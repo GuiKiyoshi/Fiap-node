7f214fdb20564b8b8ac99a56a166c8f617d22b01
Logon Aluno <logonrmlocal@fiap.com.br>
2024-02-15 08:38:30 -0300

    criando biblioteca basica

---
+++ index.js
@@ -0,0 +1,17 @@
+const sum = (a, b) => {
+    return a + b;
+}
+
+const minus = (a, b) => {
+    return a - b;
+}
+
+const mult = (a, b) => {
+    return a * b; 
+}
+
+const divide = (a, b) => {
+    return a / b;
+}
+
+module.exports = {sum, minus, mult, divide}
